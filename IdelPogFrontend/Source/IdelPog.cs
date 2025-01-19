d542991c628428a96348f637c0a8e0a86d2d82a3
Kate <84282404+Professional-Kate@users.noreply.github.com>
2025-01-19 00:43:50 +0000

    Add basic MonoGame project (#1)
    * added basic project
    
    * Created source directory and moved the two classes into it
    
    * Added background

---
+++ IdelPogFrontend/Source/IdelPog.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace IdelPogFrontend.Source
+{
+    public class IdelPog : Game
+    {
+        private GraphicsDeviceManager _graphics { get; set; }
+        private SpriteBatch _spriteBatch { get; set; }
+        
+        private Texture2D _background { get; set; }
+
+        public IdelPog()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            _background = Content.Load<Texture2D>("Images/background");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+            {
+                Exit();
+            }
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(_background, new Vector2(0, 0), Color.White);
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ IdelPogFrontend/Source/Launcher.cs
@@ -0,0 +1,12 @@
+﻿namespace IdelPogFrontend.Source
+{
+    public abstract class Launcher
+    {
+        public static void Main()
+        {
+            IdelPog idelPog = new();
+            idelPog.Run();
+        }
+    }
+}
+
